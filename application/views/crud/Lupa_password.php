a380c986953fcacc34c59335421a188fe7cc9dc2
Alfira141099 <57779369+Alfira141099@users.noreply.github.com>
2020-06-13 22:20:00 +0700

    E31180256
    Lupa Password

---
+++ application/views/crud/Lupa_password.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Lupa Password</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="<?php echo base_url('asset/home/images/icons/favicon.ico')?>"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/home/vendor/bootstrap/css/bootstrap.min.css')?>">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/home/fonts/font-awesome-4.7.0/css/font-awesome.min.css')?>">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/home/vendor/animate/animate.css')?>">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/home/vendor/css-hamburgers/hamburgers.min.css')?>">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/home/vendor/select2/select2.min.css')?>">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/home/css/util.css')?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/home/css/main.css')?>">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-pic js-tilt" data-tilt>
+					<img src="<?php echo base_url('asset/home/images/login.jpg')?>" alt="IMG">
+				</div>
+
+				<form action="#" method="post" class="login100-form validate-form">
+					<span class="login100-form-title">
+						Lupa Password ?
+					</span>
+					<div class="justify-content-center"></div>
+					<p>Tenang, anda tinggal menghubungi Admin melalui : <br>
+						</i>Email : ricemills185@gmail.com atau <br>
+						Contact Person : 0822 3492 9502
+					</p>
+				</div>
+			
+
+					
+
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	
+
+	
+<!--===============================================================================================-->	
+	<script src="<?php echo base_url('asset/home/vendor/jquery/jquery-3.2.1.min.js')?>"></script>
+<!--===============================================================================================-->
+	<script src="<?php echo base_url('asset/home/vendor/bootstrap/js/popper.js')?>"></script>
+	<script src="<?php echo base_url('asset/home/vendor/bootstrap/js/bootstrap.min.js')?>"></script>
+<!--===============================================================================================-->
+	<script src="<?php echo base_url('asset/home/vendor/select2/select2.min.js')?>"></script>
+<!--===============================================================================================-->
+	<script src="<?php echo base_url('asset/home/vendor/tilt/tilt.jquery.min.js')?>"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+<!--===============================================================================================-->
+	<script src="<?php echo base_url('asset/home/js/main.js')?>"></script>
+
+</body>
+</html>
